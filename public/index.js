6215889f6cb90ca999d3c5530d7e79b7e42ff173
Cory Baker <corybaker24@gmail.com>
2022-08-23 21:32:13 -0700

    upvote downvote

--- public/index.js
+++ public/index.js
@@ -1,26 +1,19 @@
-import { createMainContent } from './main.js';
-import { i}
+import { createMainContent } from "./main.js";
+import { fetchImage } from "./main.js";
 const initializePage = () => {
-    // Create container
-    const container = document.createElement("section");
-    container.className = "container";
-    container.style.display = "flex";
-    container.style.flexDirection = "column";
-    container.style.alignItems = "center";
-    container.style.marginTop = "20px";
-    document.body.appendChild(container);
+  // Create container
+  const container = document.createElement("section");
+  container.className = "container";
+  container.style.display = "flex";
+  container.style.flexDirection = "column";
+  container.style.alignItems = "center";
+  container.style.marginTop = "20px";
+  document.body.appendChild(container);
 };
 
 window.onload = () => {
-    initializePage();
-    createMainContent();
-
-    let addButton = document.createElement('button')
-    document.body.appendChild(addButton)
-    addButton.innerText = 'add'
-    addButton.addEventListener('click', (e) => {
-        fetchImage()
-    })
+  initializePage();
+  createMainContent();
 
 
 };
--- public/main.js
+++ public/main.js
@@ -28,3 +28,42 @@ export const fetchImage = async () => {
         console.log("Failed to fetch image", e);
     }
 };
+
+//adding new image
+let addButton = document.createElement("button");
+document.body.appendChild(addButton);
+addButton.innerText = "add";
+addButton.addEventListener("click", (e) => {
+  fetchImage();
+});
+
+
+const vote = () => {
+let clicks = 0;
+let count = document.createElement('div')
+count.innerText = clicks
+count.id = 'counter'
+document.body.appendChild(count)
+
+const upButton = document.createElement("button");
+upButton.innerText = "UpVote";
+document.body.appendChild(upButton);
+
+upButton.addEventListener("click", (e) => {
+  let countEl = document.getElementById('counter')
+  clicks += 1;
+  console.log('clicks', e.target)
+  console.log('clicks', clicks)
+  countEl.innerText = clicks
+  console.log('count', countEl)
+});
+
+const downButton = document.createElement("button");
+downButton.innerText = "DownVote";
+document.body.appendChild(downButton);
+downButton.addEventListener("click", (e) => {
+  clicks--;
+  count.innerText = clicks
+  // console.log('clicks', clicks)
+});
+}
